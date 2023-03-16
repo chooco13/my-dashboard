d33b14b7323c8398aa5c966fb317f2543c540af5
jonghoon.park <jonghoon.park@lucentblock.com>
2023-03-16 17:06:58 +0900

    add TopAppBar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,11 +8,6 @@
 			registerSW({
 				immediate: true,
 				onRegistered(r) {
-					// uncomment following code if you want check for updates
-					// r && setInterval(() => {
-					//    console.log('Checking for sw update')
-					//    r.update()
-					// }, 20000 /* 20s for testing purposes */)
 					console.log(`SW Registered: ${r}`);
 				},
 				onRegisterError(error) {
@@ -25,10 +20,10 @@
 	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
 
-<svelte:head>
-	{@html webManifest}
-</svelte:head>
-
 <main>
 	<slot />
 </main>
+
+<svelte:head>
+	{@html webManifest}
+</svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import TopAppBar from './TopAppBar.svelte';
+</script>
+
+<svelte:head>
+	<title>My Dashboard</title>
+	<meta name="description" content="Customizable Dashboard" />
+</svelte:head>
+
+<TopAppBar />
+
+<div>
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
